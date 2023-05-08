dee124607acd8de9731fe5992cbc0b2248197e69
unknown <dpark0409@gmail.com>
2023-05-08 16:34:03 -0700

    updated exercise list

--- server.js
+++ server.js
@@ -5,24 +5,47 @@ const port = 3001
 
 app.use(cors())
 
+// Exercise data
+const exercises = {
+    'Push ups': {
+        exerciseName: 'Push ups',
+        numberOfSets: 5,
+        reps: 20,
+        duration: '1:00',
+        restTime: '3:00',
+    },
+    'Sit ups': {
+        exerciseName: 'Sit ups',
+        numberOfSets: 4,
+        reps: 25,
+        duration: '2:00',
+        restTime: '3:00',
+    },
+    'Squats': {
+        exerciseName: 'Squats',
+        numberOfSets: 4,
+        reps: 15,
+        duration: '1:30',
+        restTime: '2:00',
+    },
+    'Interval running': {
+        exerciseName: 'Interval running',
+        numberOfSets: 6,
+        reps: 1,
+        duration: '4:00',
+        restTime: '1:00',
+    },
+};
+
 app.get('/api/recommended-exercise', (req, res) => {
-    const exerciseName = 'push ups'
-    const numberOfSets = 5
-    const reps = 20
-    const duration = 1
-    const restTime = 0.3
+    const { exercise } = req.query;
 
-
-    const exercise = {
-        exerciseName,
-        numberOfSets, 
-        reps,
-        duration,
-        restTime
+    if (exercise && exercises.hasOwnProperty(exercise)) {
+        res.json(exercises[exercise]);
+    } else {
+        res.status(400).json({ error: 'Invalid exercise name' });
     }
-
-    res.json(exercise);
-})
+});
 
 app.listen(port, () => {
     console.log('Microservice listening at localhost 3001');
